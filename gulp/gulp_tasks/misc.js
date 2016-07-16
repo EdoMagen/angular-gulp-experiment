d435b4950e16fc91d1735683f3ea5012f3d38db6
Edo Magen <edo.w3c@gmail.com>
2016-07-16 20:08:18 +0300

    cleaned

--- gulp/gulp_tasks/misc.js
+++ gulp/gulp_tasks/misc.js
@@ -10,6 +10,7 @@ const conf = require('../conf/gulp.conf');
 gulp.task('clean', clean);
 gulp.task('clean:docs', cleanDocs);
 gulp.task('clean:coverage', cleanCoverage);
+gulp.task('clean:all', cleanAll);
 gulp.task('notSureWhatThisFunctionDoes', notSureWhatThisFunctionDoes);
 
 function clean() {
@@ -18,12 +19,24 @@ function clean() {
     gutil.log(gutil.colors.green('Deleted \'tmp\' and \'dist\' folders'));
   });
 }
+
+function cleanAll() {
+  return del([conf.paths.dist,
+            conf.paths.tmp,
+            conf.paths.docs,
+            conf.paths.coverage]).then(paths => {
+    // this.message = paths.join('\n');
+    gutil.log(gutil.colors.green('Deleted \'tmp\', \'dist\', \'docs\' and \'coverage\' folders'));
+  });
+}
+
 function cleanDocs() {
   return del([conf.paths.docs]).then(paths => {
     // this.message = paths.join('\n');
     gutil.log(gutil.colors.green('Deleted \'docs\' folder'));
   });
 }
+
 function cleanCoverage() {
   return del([conf.paths.coverage]).then(paths => {
     // this.message = paths.join('\n');
